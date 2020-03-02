766f471bb4987c04d36f39e985654e0f905305ff
Charlie Heffernan <60932564+caheff22@users.noreply.github.com>
2020-03-02 11:17:55 -0500

    Update hangman.ting.py

--- hangman.ting.py
+++ hangman.ting.py
@@ -31,4 +31,48 @@ def main():
     
     initialize()
     getLetter()
+
+
+
+
+
+
+word = list("Howdy")
+wordList = list(word.lower())
+updatedSpaces = []
+wordLen = len(word)
+lives = 5
+letter = " "
+
+for i in range (0, int(word.en)):
+    updatedSpaces.append("-")
     
+
+
+def getLetter():
+    global letter
+    letter = raw_input("Which letter do you think is in the word")
+    
+def check():
+    global updatesSpaces
+    global lives
+    global letter
+    for x in range (0, int(word.en)):
+        if letter == word[x]:
+            updatesSpaces[x] = word[x]
+            break
+        else:
+            lives -= 1
+            if lives != 0:
+                print(updatedSpaces)
+                getLetter()
+            else:
+                print("Out of Lives. Game Over")
+    
+
+def game():
+    getLetter()
+    check()
+
+game() 
+
